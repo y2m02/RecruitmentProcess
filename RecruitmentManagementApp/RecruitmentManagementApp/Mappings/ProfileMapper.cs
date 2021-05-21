1f35af4cd344105a381cff1624b87ffe0c9bbb33
Raimy Mejia <raimym@goflowapp.com>
2021-05-21 10:45:33 -0400

    Add project

---
+++ RecruitmentManagementApp/RecruitmentManagementApp/Mappings/ProfileMapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AutoMapper;
+
+namespace TaskManagerApi.Mappings
+{
+    public class ProfileMapper : Profile
+    {
+        public ProfileMapper()
+        {
+            //CreateMap<Store, StoreResponse>()
+            //    .ForMember(
+            //        destination => destination.Used,
+            //        member => member.MapFrom(field => field.Assignments.Count > 0)
+            //    );
+
+            //CreateMap<AssignmentRequest, Assignment>();
+        }
+    }
+}
