8e94cd763adbb80d002e22ceb874bbae86b525a6
raimyy2m <raimymejia@gmail.com>
2021-05-22 15:07:05 -0400

    Add migrations

---
+++ RecruitmentManagement/RecruitmentManagementApi/Migrations/20210522190637_InitialCreate.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecruitmentManagementApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Statuses",
+                columns: table => new
+                {
+                    StatusId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statuses", x => x.StatusId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Candidates",
+                columns: table => new
+                {
+                    CandidateId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Curriculum = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GitHub = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StatusId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidates", x => x.CandidateId);
+                    table.ForeignKey(
+                        name: "FK_Candidates_Statuses_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "Statuses",
+                        principalColumn: "StatusId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recruitments",
+                columns: table => new
+                {
+                    RecruitmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    CandidateId = table.Column<int>(type: "int", nullable: false),
+                    StatusId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recruitments", x => x.RecruitmentId);
+                    table.ForeignKey(
+                        name: "FK_Recruitments_Candidates_CandidateId",
+                        column: x => x.CandidateId,
+                        principalTable: "Candidates",
+                        principalColumn: "CandidateId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Recruitments_Statuses_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "Statuses",
+                        principalColumn: "StatusId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecruitmentUpdateHistories",
+                columns: table => new
+                {
+                    RecruitmentUpdateHistoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    ChangeReason = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    RecruitmentId = table.Column<int>(type: "int", nullable: false),
+                    CandidateId = table.Column<int>(type: "int", nullable: true),
+                    StatusId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecruitmentUpdateHistories", x => x.RecruitmentUpdateHistoryId);
+                    table.ForeignKey(
+                        name: "FK_RecruitmentUpdateHistories_Candidates_CandidateId",
+                        column: x => x.CandidateId,
+                        principalTable: "Candidates",
+                        principalColumn: "CandidateId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_RecruitmentUpdateHistories_Recruitments_RecruitmentId",
+                        column: x => x.RecruitmentId,
+                        principalTable: "Recruitments",
+                        principalColumn: "RecruitmentId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RecruitmentUpdateHistories_Statuses_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "Statuses",
+                        principalColumn: "StatusId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidates_StatusId",
+                table: "Candidates",
+                column: "StatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recruitments_CandidateId",
+                table: "Recruitments",
+                column: "CandidateId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recruitments_StatusId",
+                table: "Recruitments",
+                column: "StatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecruitmentUpdateHistories_CandidateId",
+                table: "RecruitmentUpdateHistories",
+                column: "CandidateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecruitmentUpdateHistories_RecruitmentId",
+                table: "RecruitmentUpdateHistories",
+                column: "RecruitmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecruitmentUpdateHistories_StatusId",
+                table: "RecruitmentUpdateHistories",
+                column: "StatusId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RecruitmentUpdateHistories");
+
+            migrationBuilder.DropTable(
+                name: "Recruitments");
+
+            migrationBuilder.DropTable(
+                name: "Candidates");
+
+            migrationBuilder.DropTable(
+                name: "Statuses");
+        }
+    }
+}
