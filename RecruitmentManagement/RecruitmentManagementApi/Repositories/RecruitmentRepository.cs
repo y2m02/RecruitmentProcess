89aaf0a483d9ca9506618a1631213e025e2c78a5
raimyy2m <raimymejia@gmail.com>
2021-05-23 14:53:12 -0400

    Add recruitment repository

---
+++ RecruitmentManagement/RecruitmentManagementApi/Repositories/RecruitmentRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RecruitmentManagementApi.Models.Entities;
+using RecruitmentManagementApi.Repositories.Base;
+
+namespace RecruitmentManagementApi.Repositories
+{
+    public interface IRecruitmentRepository
+    {
+        Task<List<Recruitment>> GetAll();
+        Task Update(Recruitment entity);
+    }
+
+    public class RecruitmentRepository :
+        BaseRepository<Recruitment>,
+        IRecruitmentRepository
+
+    {
+        public RecruitmentRepository(RecruitmentManagementContext context) : base(context) { }
+
+        public Task<List<Recruitment>> GetAll()
+        {
+            return Context.Recruitments
+                .Include(x => x.Candidate)
+                .Include(x => x.RecruitmentUpdateHistories.OrderByDescending(r => r.Date).Skip(1))
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task Update(Recruitment entity)
+        {
+            Context.Attach(entity);
+
+            AddPropertiesToModify(
+                entity,
+                new()
+                {
+                    nameof(entity.Status),
+                    nameof(entity.Note),
+                }
+            );
+
+            await SaveChangesAndDetach(entity).ConfigureAwait(false);
+        }
+    }
+}
