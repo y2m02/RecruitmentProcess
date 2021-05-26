331be95d84cdc1d85f3cbe809999a6300c4db410
Raimy Mejia <raimym@goflowapp.com>
2021-05-26 17:02:24 -0400

    Add authorization key repository

---
+++ RecruitmentManagement/RecruitmentManagementApi/Repositories/AuthorizationKeyRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RecruitmentManagementApi.Models.Entities;
+using RecruitmentManagementApi.Repositories.Base;
+
+namespace RecruitmentManagementApi.Repositories
+{
+    public interface IAuthorizationKeyRepository
+    {
+        Task<bool> Exists(string key);
+    }
+
+    public class AuthorizationKeyRepository :
+        BaseRepository<AuthorizationKey>,
+        IAuthorizationKeyRepository
+    {
+        public AuthorizationKeyRepository(
+            RecruitmentManagementContext context
+        ) : base(context) { }
+
+        public Task<bool> Exists(string key)
+        {
+            return Context
+                .AuthorizationKeys
+                .AnyAsync(x => x.IsValid && x.Key == key);
+        }
+    }
+}
