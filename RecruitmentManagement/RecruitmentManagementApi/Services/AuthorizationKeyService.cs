7771849457bf6e0e903ebedddf97292e17a5e0bf
Raimy Mejia <raimym@goflowapp.com>
2021-05-26 17:40:34 -0400

    Add authorization key service

---
+++ RecruitmentManagement/RecruitmentManagementApi/Services/AuthorizationKeyService.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using RecruitmentManagementApi.Repositories;
+
+namespace RecruitmentManagementApi.Services
+{
+    public interface IAuthorizationKeyService
+    {
+        Task<bool> Exists(string key);
+    }
+
+    public class AuthorizationKeyService : IAuthorizationKeyService
+    {
+        private readonly IAuthorizationKeyRepository authorizationKeyRepository;
+
+        public AuthorizationKeyService(
+            IAuthorizationKeyRepository authorizationKeyRepository
+        )
+        {
+            this.authorizationKeyRepository = authorizationKeyRepository;
+        }
+
+        public Task<bool> Exists(string key)
+        {
+            return authorizationKeyRepository.Exists(key);
+        }
+    }
+}
