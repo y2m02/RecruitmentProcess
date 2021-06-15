d84dc02143670a30f3de8c11a028b9d1c3253d1e
raimyy2m <raimymejia@gmail.com>
2021-06-15 16:53:55 -0400

    Add validations

--- RecruitmentManagement/RecruitmentManagementApi/Models/Request/Candidates/CandidateRequest.cs
+++ RecruitmentManagement/RecruitmentManagementApi/Models/Request/Candidates/CandidateRequest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using HelpersLibrary.Extensions;
 using RecruitmentManagementApi.Models.Request.Base;
 
@@ -21,6 +22,19 @@ namespace RecruitmentManagementApi.Models.Request.Candidates
             if (Name.IsEmpty())
             {
                 yield return ConsumerMessages.FieldRequired.Format(nameof(Name));
+            }
+
+            if (PhoneNumber.IsNotEmpty() && (PhoneNumber.Length != 10 || !PhoneNumber.All(char.IsDigit)))
+            {
+                yield return ConsumerMessages.InvalidField.Format(nameof(PhoneNumber));
+            }
+
+            if (
+                 Email.IsNotEmpty() &&
+                (Email.Count(x => x == '@') != 1 || Email.Split('@').Any(x => x.Length < 1))
+            )
+            {
+                yield return ConsumerMessages.InvalidField.Format(nameof(Email));
             }
         }
     }
