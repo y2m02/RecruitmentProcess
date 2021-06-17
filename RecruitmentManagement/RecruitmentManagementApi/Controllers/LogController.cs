6f397a5e11240100966bd0892d21e46697d86c30
raimyy2m <raimymejia@gmail.com>
2021-06-17 10:25:37 -0400

    Add controller

---
+++ RecruitmentManagement/RecruitmentManagementApi/Controllers/LogController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RecruitmentManagementApi.Models.Enums;
+using RecruitmentManagementApi.Services;
+
+namespace RecruitmentManagementApi.Controllers
+{
+    public class LogController : BaseApiController
+    {
+        private readonly ILogService logService;
+
+        public LogController(
+            IAuthorizationKeyService authorizationKeyService,
+            ILogService logService
+        ) : base(authorizationKeyService)
+        {
+            this.logService = logService;
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public async Task<IActionResult> GetAll([FromHeader] string apiKey)
+        {
+            return await ValidateApiKey(
+                apiKey,
+                Permission.FullAccess,
+                async () => ValidateResult(await logService.GetAll().ConfigureAwait(false))
+            ).ConfigureAwait(false);
+        }
+    }
+}
